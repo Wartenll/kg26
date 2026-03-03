c95a4b701208dbe5a13f602646525cef8675a367
Wartenll <capitan9797@gmail.com>
2026-03-03 17:52:26 +0200

    03.03.26

--- task19,20,21/13964.py
+++ task19,20,21/13964.py
@@ -12,7 +12,7 @@ def f(x,y,s):
     else:
         h+=[f(x,y//2+1,s-1)]
     return any(h) if (s-1) %2 == 0 else all(h)
-print('19', [x for x in range(48,1000) if f(x,108,2)])
-print('20', [x for x in range(48,1000) if f(x,108,3) and not f(x,108,1)])
-print('21', [x for x in range(48,1000) if f(x,108,4)and not f(x,108,2)])
+print('19', [x for x in range(48,1000) if f(x,60,2)])
+print('20', [x for x in range(48,1000) if f(x,60,3) and not f(x,60,1)])
+print('21', [x for x in range(48,1000) if f(x,60,4)and not f(x,60,2)])
 
